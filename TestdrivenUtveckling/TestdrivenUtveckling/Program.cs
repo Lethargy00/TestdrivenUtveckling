8c9eef3931f437d7c92eb4de61c99d8c84a2ae4e
Lethargy00 <142389181+Lethargy00@users.noreply.github.com>
2023-11-28 10:55:52 +0100

    Added stuff

--- TestdrivenUtveckling/TestdrivenUtveckling/Program.cs
+++ TestdrivenUtveckling/TestdrivenUtveckling/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System.Reflection.Metadata;
+
+public interface IDatabase
+{
+    public void AddUser(User user);
+    public void RemoveUser(int userId);
+    public User GetUser(int userId);
+}
+
+// Define the User class
+public class User
+{
+    // Define properties of the User class
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+
+}
+
+// Implement the UserManager class
+public class UserManager : IDatabase
+{
+    private readonly IDatabase _database;
+
+    // Constructor injection to pass IDatabase dependency
+    public UserManager(IDatabase database)
+    {
+        _database = database ?? throw new ArgumentNullException(nameof(database));
+    }
+
+    public void AddUser(User user)
+    {
+        _database.AddUser(user);        
+    }
+
+    public void RemoveUser(int userId)
+    {
+        _database.RemoveUser(userId);
+    }
+
+    public User GetUser(int userId)
+    {
+        return _database.GetUser(userId);
+    }
+}
---
+++ TestdrivenUtveckling/TestdrivenUtveckling/Tests/TestUserManager.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common.Utilities;
+using NUnit.Framework;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Legacy;
+
+namespace TestdrivenUtveckling.Tests
+{
+    [TestFixture]
+    internal class TestUserManager
+    {
+        private Mock<IDatabase> _mockDatabase;
+        private UserManager _userManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockDatabase = new Mock<IDatabase>();
+            _userManager = new UserManager(_mockDatabase.Object);
+        }
+
+        [TearDown] 
+        public void Teardown()
+        {
+            // Clean up resources after each test if needed.
+        }
+
+        [Test]
+        public void TestAddUser()
+        {
+            // Arrange
+            var user = new User
+            {
+                UserId = 1,
+                UserName = "Test"
+            };
+
+            // Act
+            _userManager.AddUser(user);
+
+            // Assert
+            _mockDatabase.Verify(db => db.AddUser(user), Times.Once);
+        }
+
+        [Test]
+        public void TestRemoveUser()
+        {
+            // Arrange
+            var userId = 1;
+
+            // Act
+            _userManager.RemoveUser(userId);
+
+            // Assert
+            _mockDatabase.Verify(db => db.RemoveUser(userId), Times.Once);
+        }
+
+        [Test]
+        public void TestGetUser()
+        {
+            // Arrange
+            var userId = 1;
+            var expectedUser = new User
+            {
+                UserId = 1,
+                UserName = "Test"
+            };
+            _mockDatabase.Setup(db => db.GetUser(userId)).Returns(expectedUser);
+
+            // Act 
+            var result = _userManager.GetUser(userId);
+
+            // Assert
+            ClassicAssert.AreEqual(expectedUser, result);
+        }
+    }
+}
