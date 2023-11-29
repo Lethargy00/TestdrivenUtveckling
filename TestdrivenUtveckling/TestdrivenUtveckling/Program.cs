c907484089265ff0db69e54d110e3cfc502e465a
Lethargy00 <142389181+Lethargy00@users.noreply.github.com>
2023-11-29 13:16:49 +0100

    Updated comments

--- TestdrivenUtveckling/TestdrivenUtveckling/Program.cs
+++ TestdrivenUtveckling/TestdrivenUtveckling/Program.cs
@@ -1,49 +1,87 @@
 ﻿using System.Reflection.Metadata;
 
+// Interface representing a database with user-related operations.
 public interface IDatabase
 {
+    /// <summary>
+    /// Adds a new user to the database.
+    /// </summary>
+    /// <param name="user">The user to be added.</param>
     public void AddUser(User user);
+
+    /// <summary>
+    /// Removes a user from the database based on the user's ID.
+    /// </summary>
+    /// <param name="userId">The ID of the user to be removed.</param>
     public void RemoveUser(int userId);
+
+    /// <summary>
+    /// Retrieves a user from the database based on the user's ID.
+    /// </summary>
+    /// <param name="userId">The ID of the user to be retrieved.</param>
+    /// <returns>The user with the specified ID, or null if not found.</returns>
     public User GetUser(int userId);
 }
 
-// Define the User class
+/// <summary>
+/// Represents a user in the system.
+/// </summary>
 public class User
 {
-    // Define properties of the User class
+    /// <summary>
+    /// Gets or sets the unique identifier of the user.
+    /// </summary>
     public int UserId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the user.
+    /// </summary>
     public string UserName { get; set; }
 
 }
 
-// Implement the UserManager class
+/// <summary>
+/// Implement the UserManager class
+/// Manages user-related operations using an underlying database.
+/// </summary>
 public class UserManager : IDatabase
 {
     private readonly IDatabase _database;
 
     // Constructor injection to pass IDatabase dependency
+    /// <summary>
+    /// Initializes a new instance of the UserManager class.
+    /// </summary>
+    /// <param name="database">The database used for user-related operations.</param>
+    /// <exception cref="ArgumentNullException"></exception>
     public UserManager(IDatabase database)
     {
+        // If the database is null, throw and ArgumentNullException.
         _database = database ?? throw new ArgumentNullException(nameof(database));
     }
 
     public void AddUser(User user)
     {
-        if (user == null)
-        {
-            throw new ArgumentNullException(nameof(user));
-        }
+        // If the user is null, throw an ArgumentNullException.
+        ArgumentNullException.ThrowIfNull(user);
 
+        // Add the user to the database.
         _database.AddUser(user);        
     }
 
     public void RemoveUser(int userId)
     {
+        // Remove the user from the database.
         _database.RemoveUser(userId);
     }
 
     public User GetUser(int userId)
     {
-        return _database.GetUser(userId);
+
+        // Gets user Id or sends an exception message if it's null.
+        var user = _database.GetUser(userId) ?? throw new ArgumentException($"User with ID {userId} does not exist.");
+
+        // Retrieve the user from the database.
+        return user;
     }
 }
--- TestdrivenUtveckling/TestdrivenUtveckling/Tests/TestUserManager.cs
+++ TestdrivenUtveckling/TestdrivenUtveckling/Tests/TestUserManager.cs
@@ -16,6 +16,9 @@ namespace TestdrivenUtveckling.Tests
         private Mock<IDatabase> _mockDatabase;
         private UserManager _userManager;
 
+        /// <summary>
+        /// Creates a mock database and user manager for each test.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
@@ -23,54 +26,77 @@ namespace TestdrivenUtveckling.Tests
             _userManager = new UserManager(_mockDatabase.Object);
         }
 
+        /// <summary>
+        /// Verify that all expectations were met.
+        /// </summary>
         [TearDown]
         public void Teardown()
         {
             _mockDatabase.VerifyAll();
         }
 
+        /// <summary>
+        /// Test that AddUser calls AddUser on the database exactly once.
+        /// </summary>
         [Test]
         public void TestAddUser()
         {
-            // Arrange
+            // Arrange: create a test user.
             var user = new User { UserId = 1, UserName = "TestUser" };
             _mockDatabase.Setup(db => db.AddUser(user));
 
-            // Act
+            // Act: call AddUser on the user manager.
             _userManager.AddUser(user);
 
-            // Assert
+            // Assert: Verify that AddUser was called on the database.
             _mockDatabase.Verify(db => db.AddUser(user), Times.Once);
         }
 
+        /// <summary>
+        /// Test that RemoveUser calls RemoveUser on the database exactly once.
+        /// </summary>
         [Test]
         public void TestRemoveUser()
         {
-            // Arrange
+            // Arrange: created a test user ID.
             var userId = 1;
             _mockDatabase.Setup(db => db.RemoveUser(userId));
 
-            // Act
+            // Act: call RemoveUser on the user manager.
             _userManager.RemoveUser(userId);
 
-            // Assert
+            // Assert: verify that RemoveUser was called on the database.
             _mockDatabase.Verify(db => db.RemoveUser(userId), Times.Once);
         }
 
+        /// <summary>
+        /// Test that GetUser calls GetUser on the database exactly once and returns the correct user.
+        /// </summary>
         [Test]
         public void TestGetUser()
         {
-            // Arrange
+            // Arrange: create a test user ID and user.
             var userId = 1;
             var user = new User { UserId = userId, UserName = "TestUser" };
             _mockDatabase.Setup(db => db.GetUser(userId)).Returns(user);
 
-            // Act
+            // Act: call GetUser on the user manager.
             var result = _userManager.GetUser(userId);
 
-            // Assert
+            // Assert: verify that GetUser was called on the database and the returned user is correct.
             ClassicAssert.AreEqual(user, result);
             _mockDatabase.Verify(db => db.GetUser(userId), Times.Once);
+        }
+
+        [Test] 
+        public void TestRemoveUser_UserDoesNotExist()
+        {
+            // Arrange: create a test user ID.
+            var userId = 1;
+            _mockDatabase.Setup(db => db.GetUser(userId)).Returns((User)null);
+
+            // Act and Assert: verify that an exception is thrown.
+            ClassicAssert.Throws<ArgumentException>(() => _userManager.RemoveUser(userId));
         }
 
 
